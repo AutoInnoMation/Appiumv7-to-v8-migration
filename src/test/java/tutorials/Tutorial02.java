a999c174f8628f2e41d271654f66e92b55c64bfc
AutoInnoMation <90962838+AutoInnoMation@users.noreply.github.com>
2022-06-19 23:35:30 +0100

    Update Tutorial02.java
    Adding Zoom In and Zoom Out method

--- src/test/java/tutorials/Tutorial02.java
+++ src/test/java/tutorials/Tutorial02.java
@@ -35,7 +35,7 @@ public class Tutorial02 {
                 .eventTimings();
 
         try {
-            driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub/"), options);
+            driver = new AppiumDriver(new URL("http://127.0.0.1:4723"), options);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -43,9 +43,9 @@ public class Tutorial02 {
     }
 
     @Test
-    public void doTest(){
+    public void zoomIn(){
 
-            driver.findElement(AppiumBy.accessibilityId("Photo taken on Jun 10, 2022 8:54:15 PM")).click();
+        driver.findElement(AppiumBy.accessibilityId("Photo taken on Jun 10, 2022 8:54:15 PM")).click();
 
         WebElement ele01 = driver.findElement(By.id("com.google.android.apps.photos:id/video_player_controller_fragment_container"));
 
@@ -95,7 +95,7 @@ public class Tutorial02 {
         swipe02.addAction(finger2.createPointerMove(Duration.ofMillis(500),
                 PointerInput.Origin.viewport(),finger2End, centerY));
         //Get up Finger from Screen
-        swipe02.addAction(finger2.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        swipe02.addAction(finger2.createPointerUp(0));
 
         //Perform the actions
         driver.perform(Arrays.asList(swipe01,swipe02));
@@ -108,5 +108,70 @@ public class Tutorial02 {
 
     }
 
+    @Test
+    public void zoomOut(){
+
+        //driver.findElement(AppiumBy.accessibilityId("Photo taken on Jun 10, 2022 8:54:15 PM")).click();
+
+        WebElement ele01 = driver.findElement(By.id("com.google.android.apps.photos:id/video_player_controller_fragment_container"));
+
+        //Get the centre of the element on the horizontal axis
+        int centerX = ele01.getRect().x + (ele01.getSize().width/2);
+        int centerY = ele01.getRect().y + (ele01.getSize().height/2);
+
+        //Determine our movement in the pixels on the horizontal axis
+        int xMovement = 300;
+
+        //Set the starting position for both fingers
+        //Don't start at the exact center move x pixels to the left/right from the center
+        int finger1Start = centerX - (2*xMovement);
+        int finger2Start = centerX +  (2*xMovement);
+
+        //Set the end position for the both fingers we already moved Xpx from the center to start, now Xpx again to the left/right
+        int finger1End  = centerX - xMovement;
+        int finger2End  = centerX + xMovement;
+
+
+        //Finger 1
+        //Type of Pointer Input
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH,"finger1");
+        //Creating Sequence object to add actions
+        Sequence swipe01 = new Sequence(finger1,1);
+        //Move finger into starting position
+        swipe01.addAction(finger1.createPointerMove(Duration.ofSeconds(0),PointerInput.Origin.viewport(),finger1Start,centerY));
+        //Finger comes down into contact with screen
+        swipe01.addAction(finger1.createPointerDown(0));
+        //Finger moves to end position
+        swipe01.addAction(finger1.createPointerMove(Duration.ofMillis(500),
+                PointerInput.Origin.viewport(),finger1End, centerY));
+        //Get up Finger from Screen
+        swipe01.addAction(finger1.createPointerUp(0));
+
+
+        //Finger 2
+        //Type of Pointer Input
+        PointerInput finger2 = new PointerInput(PointerInput.Kind.TOUCH,"finger2");
+        //Creating Sequence object to add actions
+        Sequence swipe02 = new Sequence(finger2,1);
+        //Move finger into starting position
+        swipe02.addAction(finger2.createPointerMove(Duration.ofSeconds(0),PointerInput.Origin.viewport(),finger2Start,centerY));
+        //Finger comes down into contact with screen
+        swipe02.addAction(finger2.createPointerDown(0));
+        //Finger moves to end position
+        swipe02.addAction(finger2.createPointerMove(Duration.ofMillis(500),
+                PointerInput.Origin.viewport(),finger2End, centerY));
+        //Get up Finger from Screen
+        swipe02.addAction(finger2.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        //Perform the actions
+        driver.perform(Arrays.asList(swipe01,swipe02));
+
+        try {
+            Thread.sleep(3000);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
 
 }
